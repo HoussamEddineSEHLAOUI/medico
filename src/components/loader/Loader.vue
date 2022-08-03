5047a662a7311b8f8aa8a15a1508aa54e45f0058
Houssam <houssam@infine.io>
2022-08-03 10:29:10 +0200

    Add nav side chat bars

---
+++ src/components/loader/Loader.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- loader Start -->
+  <div id="loading" :style="style">
+    <div id="loading-center">
+    </div>
+  </div>
+  <!-- loader END -->
+</template>
+<script>
+import lightLogo from '@/assets/images/loader.png'
+import darkLogo from '@/assets/images/logo-white.png'
+export default {
+  name: 'LoadeR',
+  props: {
+    dark: { type: Boolean, default: false }
+  },
+  mounted () {
+    this.style = `background: #fff url(${this.logo}) no-repeat scroll center center;`
+  },
+  data () {
+    return {
+      logo: lightLogo,
+      darkLogo: darkLogo,
+      style: `background: #fff url(${darkLogo}) no-repeat scroll center center;background-size:10%;`
+    }
+  }
+}
+</script>
---
+++ src/layouts/Layout.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <!-- <LoadeR /> -->
+    <div class="wrapper">
+      <!-- Sidebar  -->
+      <SideBar></SideBar>
+      <!-- TOP Nav Bar -->
+      <NavBar></NavBar>
+       <!-- Right Nav Bar -->
+      <RightSideBar toggleClass="top-50 setting-toggle iq-card">
+        <i class="ri-sound-module-fill font-size-18 text-primary" />
+      </RightSideBar>
+    </div>
+    <FooterStyle1>
+      <template v-slot:left>
+        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+        <li class="list-inline-item"><a href="#">Terms of Use</a></li>
+      </template>
+      <template v-slot:right>
+        Copyright 2020 <a href="#">SocialV</a> All Rights Reserved.
+      </template>
+    </FooterStyle1>
+  </div>
+</template>
+
+<script>
+// import LoadeR from './loader/Loader'
+import NavBar from '../components/navbar/NavBar.vue'
+import SideBar from '../components/sidebar/SideBar.vue'
+import RightSideBar from '../components/rightsidebar/RightSideBar'
+import FooterStyle1 from '../components/footer/FooterStyle1'
+export default {
+  name: 'LayouT',
+  components: {
+    NavBar,
+    SideBar,
+    RightSideBar,
+    FooterStyle1
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
